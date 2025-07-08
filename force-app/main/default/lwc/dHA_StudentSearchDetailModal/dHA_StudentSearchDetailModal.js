6cdc8d70781b325dccb81abf9afc08d541d6dd78
HienPhan <Hienphan01031998@gmail.com>
2025-07-08 11:19:33 +0700

    Build functions for StudyResult on Apex, JS

--- force-app/main/default/lwc/dHA_StudentSearchDetailModal/dHA_StudentSearchDetailModal.js
+++ force-app/main/default/lwc/dHA_StudentSearchDetailModal/dHA_StudentSearchDetailModal.js
@@ -6,6 +6,10 @@ import ADDRESS_FIELD from "@salesforce/schema/DHA_Student__c.Address__c";
 import STUDENT_CODE_FIELD from "@salesforce/schema/DHA_Student__c.Student_Code__c";
 import AGE_FIELD from "@salesforce/schema/DHA_Student__c.Age__c";
 import EMAIL_FIELD from "@salesforce/schema/DHA_Student__c.Email__c";
+import { wire } from "lwc";
+import countAssignedClassesOfStudent from "@salesforce/apex/StudentClassController.countAssignedClassesOfStudent";
+import countClassHaveFinalScore from "@salesforce/apex/StudentClassController.countClassHaveFinalScore";
+import getAverageScores from "@salesforce/apex/StudentClassController.getAverageScores";
 
 import { api } from "lwc";
 import LightningModal from "lightning/modal";
@@ -22,5 +26,20 @@ export default class DHA_StudentSearchDetailModal extends LightningModal {
   emailField = EMAIL_FIELD;
   @api recordId;
 
+  @wire(countAssignedClassesOfStudent, { studentId: '$recordId' })
+  assignedClasses;
 
+  @wire(countClassHaveFinalScore, { studentId: '$recordId' })
+  classesHaveFinalScore;
+
+  @wire(getAverageScores, { studentId: '$recordId' })
+  wiredGetAverageScores({ data, error }) {
+    if (data) {
+      if (data.length === 0) {
+        this.averageFinalScore = "0";
+      } else { this.averageFinalScore = data.join(', '); }
+    } else if (error) {
+      console.log("Error: " + JSON.stringify(error));
+    }
+  }
 }
